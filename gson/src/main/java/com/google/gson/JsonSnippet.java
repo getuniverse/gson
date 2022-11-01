9bbde721b9957d94daf9451afee21752f911d927
Tibor Varga <tibor.varga@happeo.com>
2022-11-02 01:34:21 +0200

    JSON snippet NULL handling

--- gson/src/main/java/com/google/gson/JsonSnippet.java
+++ gson/src/main/java/com/google/gson/JsonSnippet.java
@@ -1,23 +1,22 @@
 package com.google.gson;
 
-import java.util.Objects;
-
 /**
- * Instances of this class encapsulate a pre-encoded JSON snippet. Use this
- * class to exclude parts, or the entirety, of a JSON input or output from
- * automatic object mapping.
+ * Instances of this class encapsulate a pre-encoded JSON snippet. Use this class to exclude parts, or the entirety, of
+ * a JSON input or output from automatic object mapping.
  * <p>
- * Targeted specifically for APIs that deal with client-controlled pieces of
- * data within server-controlled resource state, use of this class allows
+ * Targeted specifically for APIs that deal with client-controlled pieces of data within server-controlled resource
+ * state, use of this class allows
  * <em>partially</em> mapping JSON to objects and vice versa.
  * <p>
- * Partial mapping allows a mapped object to receive parts of the input in its
- * original JSON form without building an object graph therefrom: you get the
- * validated, but un-parsed JSON snippet to e.g. save in a database.
+ * Partial mapping allows a mapped object to receive parts of the input in its original JSON form without building an
+ * object graph therefrom: you get the validated, but un-parsed JSON snippet to e.g. save in a database.
  * <p>
- * Partial mapping also allows adding un-parsed JSON, possibly coming from the
- * database where the partial JSON text was previously saved, to an object
- * mapped to JSON.
+ * Partial mapping also allows adding un-parsed JSON, possibly coming from the database where the partial JSON text was
+ * previously saved, to an object mapped to JSON.
+ * <p>
+ * NULL JSON values are represented by Java <code>null</code> values: both <code>JsonSnippet.with(null)</code> and
+ * <code>JsonSnippet.with("null")</code> return <code>null</code>, which means that <code>new Gson().fromJson("null",
+ * JsonSnippet.class)</code> will also return <code>null</code>.
  */
 public final class JsonSnippet {
 
@@ -26,10 +25,11 @@ public final class JsonSnippet {
      *
      * @param contents the snippet to encapsulate; may be <code>null</code>.
      *
-     * @return a new instance; never <code>null</code>.
+     * @return a new instance; may be <code>null</code>.
      */
     public static JsonSnippet with(final CharSequence contents) {
-        return new JsonSnippet(contents);
+        final String value = contents == null ? "null" : contents.toString();
+        return value.equals("null") ? null : new JsonSnippet(value);
     }
 
     /**
@@ -37,16 +37,16 @@ public final class JsonSnippet {
      *
      * @param snippet deserialized JSON snippet; may be <code>null</code>.
      *
-     * @return the JSON snippet; never <code>null</code>.
+     * @return the JSON snippet; may be <code>null</code>.
      */
     public static String json(final JsonSnippet snippet) {
-        return snippet == null ? "null" : snippet.contents;
+        return snippet == null ? null : snippet.contents;
     }
 
     private final String contents;
 
-    private JsonSnippet(final CharSequence contents) {
-        this.contents = String.valueOf(contents);
+    private JsonSnippet(final String contents) {
+        this.contents = contents;
     }
 
     @Override
@@ -65,7 +65,7 @@ public final class JsonSnippet {
 
     @Override
     public int hashCode() {
-        return Objects.hash(contents);
+        return contents.hashCode();
     }
 
     @Override
--- gson/src/test/java/com/google/gson/functional/JsonSnippetTest.java
+++ gson/src/test/java/com/google/gson/functional/JsonSnippetTest.java
@@ -4,6 +4,7 @@ import static com.google.gson.JsonSnippet.json;
 import static com.google.gson.ToNumberPolicy.LONG_OR_DOUBLE;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertNull;
 
 import java.util.List;
 import java.util.Map;
@@ -35,14 +36,13 @@ public final class JsonSnippetTest {
 
         assertThat(GSON.toJson(JsonSnippet.with(null)), is(json));
         assertThat(GSON.toJson(JsonSnippet.with(json)), is(json));
-        assertThat(json(GSON.fromJson(json, JsonSnippet.class)), is(json));
+        assertNull(json(GSON.fromJson(json, JsonSnippet.class)));
     }
 
     @Test
     public void serializationParts() {
         final JsonSnippet _true = JsonSnippet.with("true");
         final JsonSnippet _false = JsonSnippet.with("false");
-        final JsonSnippet _null = JsonSnippet.with("null");
         final JsonSnippet _int = JsonSnippet.with("1234");
         final JsonSnippet _string = JsonSnippet.with("\"abcd\"");
         final JsonSnippet _list = JsonSnippet.with(GSON.toJson(List.of(1, 2)));
@@ -51,7 +51,7 @@ public final class JsonSnippetTest {
 
         final String json = GSON.toJson(Map.of("a", _true,
                                                "b", _false,
-                                               "c", _null,
+                                               "c", "null",
                                                "d", _int,
                                                "e", _string,
                                                "f", _list,
@@ -107,11 +107,11 @@ public final class JsonSnippetTest {
 
         assertThat(r1.x, is("text"));
         assertThat(r1.y, is(12345678L));
-        assertThat(json(r1.d), is("null"));
+        assertNull(json(r1.d));
 
         assertThat(r1.r2.a, is("a"));
         assertThat(r1.r2.b, is(0L));
-        assertThat(json(r1.r2.d), is("null"));
+        assertNull(json(r1.r2.d));
     }
 
     public record R1(String x, JsonSnippet d, long y, R2 r2) {}
