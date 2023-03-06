6d4ae45ee8bc3274a049987fffeeb82bc58662b5
Maicol <79454487+MaicolAntali@users.noreply.github.com>
2023-03-06 17:24:09 +0100

    Fix #2334 (#2337)
    * Fix #2334
    
    This commit replaces the `NumberFormatException` with `MalformedJsonException` in the `JsonReader#readEscapeCharacter()` and also fixes the tests.
    
    * Removes white-space

--- gson/src/main/java/com/google/gson/stream/JsonReader.java
+++ gson/src/main/java/com/google/gson/stream/JsonReader.java
@@ -1591,7 +1591,7 @@ public class JsonReader implements Closeable {
    * been read. This supports both unicode escapes "u000A" and two-character
    * escapes "\n".
    *
-   * @throws NumberFormatException if any unicode escape sequences are
+   * @throws MalformedJsonException if any unicode escape sequences are
    *     malformed.
    */
   @SuppressWarnings("fallthrough")
@@ -1618,7 +1618,7 @@ public class JsonReader implements Closeable {
         } else if (c >= 'A' && c <= 'F') {
           result += (c - 'A' + 10);
         } else {
-          throw new NumberFormatException("\\u" + new String(buffer, pos, 4));
+          throw new MalformedJsonException("\\u" + new String(buffer, pos, 4));
         }
       }
       pos += 4;
