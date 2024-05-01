e3af0dbdd7dc612494f3f868907bc11a4d598095
Éamonn McManus <emcmanus@google.com>
2024-05-01 07:24:29 -0700

    Fix a Yoda condition to placate Error Prone. (#2673)

--- gson/src/main/java/com/google/gson/stream/JsonReader.java
+++ gson/src/main/java/com/google/gson/stream/JsonReader.java
@@ -1181,7 +1181,7 @@ public class JsonReader implements Closeable {
     }
 
     String result =
-        (null == builder) ? new String(buffer, pos, i) : builder.append(buffer, pos, i).toString();
+        (builder == null) ? new String(buffer, pos, i) : builder.append(buffer, pos, i).toString();
     pos += i;
     return result;
   }
